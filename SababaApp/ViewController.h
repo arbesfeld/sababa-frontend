56b6b0230221d338be5c3dd7bbcefc6e13484a9a
arbesfeld <arbesfeld@gmail.com>
2013-12-30 04:35:10 -0500

    Update

--- SababaApp/ViewController.h
+++ SababaApp/ViewController.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *englishButtonOne;
+@property (weak, nonatomic) IBOutlet UIButton *frenchButtonOne;
+@property (weak, nonatomic) IBOutlet UIButton *hebrewButtonOne;
+@property (weak, nonatomic) IBOutlet UIButton *englishButtonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *frenchButtonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *hebrewButtonTwo;
 
 @end
